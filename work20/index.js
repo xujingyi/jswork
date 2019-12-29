302647c6e064a198c0007fe540ab2226a7bc21c4
xujingyi <2232069899@qq.com>
2019-12-29 17:08:46 +0800

    work20

--- work20/index.js
+++ work20/index.js
@@ -1,13 +1,12 @@
-//默认情况下，显示第一个分类下的菜单
 $('#fold>ul>li:first').find('.wrap').css({
     display: 'block'
 });
-//根据用户单击，折叠或展开对应的菜单
-$('#fold>ul>li:first').click(function() {
+
+$('#fold>ul>li').click(function () {
     $(this).siblings('li').find('.wrap').css({
-    display: 'block'
+        display: 'none'
     });
     $(this).find('.wrap').css({
         display: 'block'
     });
-});    
+});
