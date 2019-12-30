e0210e604d7ccac038b7757915bcee88069ffbe0
xujingyi <2232069899@qq.com>
2019-12-30 16:34:13 +0800

    work20

--- work20/index.js
+++ work20/index.js
@@ -1,8 +1,6 @@
-//默认情况下，显示第一个分类下的菜单
 $('#fold>ul>li:first').find('.wrap').css({
     display: 'block'
 });
-//根据用户点击，折叠或展开对应的菜单
 $('#fold>ul>li').click(function () {
     $(this).siblings('li').find('.wrap').css({
         display: 'none'
