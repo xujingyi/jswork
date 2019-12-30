8e9d3dde42489b044516875150fd33a1e1b14e09
xujingyi <2232069899@qq.com>
2019-12-30 16:41:02 +0800

    work20-1

--- work20-1/index1.js
+++ work20-1/index1.js
@@ -1,13 +1,12 @@
-//获取按钮添加点击事件，获取第一个下拉框中被选中的opttion添加到第二个下拉框
-$('#toRight').click(function () {//右移
+$('#toRight').click(function () {
     $('#right>select').append($('#left>select>option:selected'));
-});
-$('#toLeft').click(function () {//左移
+ });
+ $('#toLeft').click(function () {
     $('#left>select').append($('#right>select>option:selected'));
-});
-$('#toAllRight').click(function () {//全部右移
+ });
+ $('#toAllRight').click(function () {
     $('#right>select').append($('#left>select>option'));
-});
-$('#toAllLeft').click(function () {//全部左移
+ });
+ $('#toAllLeft').click(function () {
     $('#left>select').append($('#right>select>option'));
-});
+ });
