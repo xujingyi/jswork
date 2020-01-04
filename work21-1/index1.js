63c96c1f02ec01c0a59729fd4092f0d83659ebb6
xujingyi <2232069899@qq.com>
2020-01-04 21:13:19 +0800

    work21

--- work21-1/index1.js
+++ work21-1/index1.js
@@ -1,6 +1,6 @@
-$("input:last").click(function(){
-    $.post("/ajax",$('form').serialize(),function(res){
+$('input:last').click(function () {
+    $.post('/ajax',$('form').serialize(),function(res){
         result = res.reverse().map((val)=>{return JSON.stringify(val)+"<br>"})
-        $("#result").empty().append($("<div></div>").html(result))
+        $('#result').empty().append($('<div></div>').html(result))
     })
 })
--- work21/index.js
+++ work21/index.js
@@ -4,18 +4,14 @@ $(function () {
     var delay = 1000;
     var width = 1420;
     var speed = 400;
-
     var firstimg = $('.hot li').first().clone();
     $('.hot').append(firstimg).width($('.hot li').length * width);
-
     timer = setInterval(imgChange, delay);
-
     $('.banner').hover(function () {
         clearInterval(timer);
     }, function () {
         timer = setInterval(imgChange, delay);
     });
-
     $('.dot li').mouseover(function () {
         i = $(this).index();
         $('.hot').stop().animate({
@@ -23,22 +19,19 @@ $(function () {
         }, 200);
         dotChange();
     });
-
     $('.banner').hover(function () {
         $('.arrow').show();
     }, function () {
         $('.arrow').hide();
     });
-
     $('.next').click(function () {
         imgChange()
     });
-
     $('.prev').click(function () {
         --i;
         if (i == -1) {
             i = $('.hot li').length - 2;
-            $('.hot').css({
+            $('hot').css({
                 left: -($('.hot li').length - 1) * width
             });
         }
@@ -47,13 +40,11 @@ $(function () {
         }, speed);
         dotChange();
     });
-
     function imgChange() {
         ++i;
         isCrack();
         dotChange();
     }
-
     function isCrack() {
         if (i == $('.hot li').length) {
             i = 1;
@@ -65,12 +56,11 @@ $(function () {
             left: -i * width
         }, speed);
     }
-
     function dotChange() {
-        if (i == $('hot li').length - 1) {
+        if (i == $('.hot li').length - 1) {
             $('.dot li').eq(0).addClass('on').siblings().removeClass('on');
         } else {
             $('.dot li').eq(i).addClass('on').siblings().removeClass('on');
         }
     }
-})
+});
